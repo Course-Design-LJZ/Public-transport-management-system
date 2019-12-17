3f8ba60c77d23a485804edb0d9990f12ba61dff1
aether <1025313354@qq.com>
2019-12-17 12:55:15 +0800

    add Graph

--- Transport_System/Graph.h
+++ Transport_System/Graph.h
@@ -1,3 +1,44 @@
 #pragma once
-#include <iostream>
-#include <cstdio>
+
+#include <vector>
+#include <stack>
+#include <map>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+#define _OK_ 1
+#define _NO_ 0
+
+#define VI vector
+
+class line {
+
+};
+
+class Graph {
+public:
+	struct edge {
+		int v, w, line;
+		edge(int v, int w, int line) {
+			this->v = v;
+			this->w = w;
+			this->line = line;
+		}
+	};
+
+	VI <VI <edge>> G;
+	VI <bool> vis;
+	stack <int> Available;
+
+	int Ins();
+
+	void Ins_Edge(int u, int v, int w, int c);
+
+	int Del(int u);
+
+	int Del_Edge(int u, int v);
+
+	int change(int u, int v, int w, int c);
+};
